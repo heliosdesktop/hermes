c131324ea077f9c3997ce71f0d49056a8b5a593e
Nahuel Alejandro Veron <nahuelav96@gmail.com>
2026-04-28 19:51:33 -0300

    ci: Add Windows build targets and scripts
    Introduce Windows runner matrix and split checkout/build steps by OS.
    Add scripts/checkout_hermes.ps1 and scripts/build_hermes_windows.ps1
    (which uses MSVC_ARCH). Expand Linux install step to include
    build-essential, libicu-dev, python3, tzdata and libreadline-dev.

---
+++ scripts/build_hermes_windows.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = "Stop"
+
+$build_type = if (($env:BUILD_TYPE ?? "").ToLower() -eq "release") {
+    "Release"
+} else {
+    "Debug"
+}
+
+switch ($env:MSVC_ARCH) {
+    "x64"   { $cmakeArch = "x64" }
+    "arm64" { $cmakeArch = "ARM64" }
+    default { throw "Unknown MSVC_ARCH: $env:MSVC_ARCH" }
+}
+
+cmake -S hermes -B build -G "Visual Studio 17 2022" -A $cmakeArch
+cmake --build build --config $build_type --parallel
---
+++ scripts/checkout_hermes.ps1
@@ -0,0 +1,1 @@
+git clone --depth 1 --branch $env:HERMES_REF https://github.com/facebook/hermes.git
